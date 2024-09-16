ac9a0ba8949062198c157f7e23f1604277538241
Leonardo <leonardo.mf@ufu.br>
2024-09-16 10:40:19 -0300

    update site files

--- docs/site/assets/scripts/loadcontent.js
+++ docs/site/assets/scripts/loadcontent.js
@@ -1,19 +1,21 @@
-const botao = document.getElementById("btn");
-const contentDiv = document.getElementById("ctn-1");
-const titulo = document.getElementById("titulo");
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.getElementById("btn-doc-visao");
+    const contentSection = document.getElementById("content-section-doc-visao");
+    const contentCard = document.getElementById("doc-visao-card")
 
-const displayDoTitulo = titulo.style.display;
+    botao.addEventListener("click", function (){
+        fetch('./content/docvisao.html').then(function (response) {
+            if (response.ok) {
+                return response.text();
+            }
+            contentSection.appendChild(document.createElement("p").innerText("ERRO"))
+            throw response;
+        }).then(function (text) {
+            contentCard.style.margin = 0;
+            contentSection.innerHTML = text;
+        })
+    });
+})
 
-botao.addEventListener("click", function (){
-    fetch('/content.html').then(function (response) {
-        if (response.ok) {
-            return response.text();
-        }
-        throw response;
-    }).then(function (text) {
-        titulo.style.display = "none"
-        contentDiv.innerHTML = text;
-    })
-});
 
 // https://gomakethings.com/how-to-load-html-from-another-page-with-vanilla-javascript/
