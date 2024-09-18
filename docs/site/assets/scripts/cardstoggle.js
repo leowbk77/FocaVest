a1565e9acd9bac98565118a17a6fce042b1e68fd
Leonardo <leonardo.mf@ufu.br>
2024-09-17 23:50:34 -0300

    update site

---
+++ docs/site/assets/scripts/cardstoggle.js
@@ -0,0 +1,41 @@
+let cards = document.querySelectorAll('.card');
+const isCollapsed = 'is-collapsed';
+const isExpanded = 'is-expanded';
+const isInactive = 'is-inactive';
+
+// controla o toggle dos cards
+function toggle(card) {
+    // verifica se o card está colapsado
+    if (card.classList.contains(isCollapsed)) {
+        cards.forEach(function (c) {
+            // colapsa outros cards expandidos e os torna inativos
+            if (c !== card) {
+                c.classList.remove(isExpanded);
+                c.classList.add(isCollapsed, isInactive);
+            }
+        });
+        // expande o card clicado
+        card.classList.remove(isCollapsed);
+        card.classList.add(isExpanded);
+    } else {
+        // do contrário colapsa o card aberto e remove a inatividade dos outros cards
+        card.classList.remove(isExpanded);
+        card.classList.add(isCollapsed);
+        cards.forEach(function (c) {
+            c.classList.remove(isInactive)
+        });
+    }
+}
+
+// aciona o toggle em cada card
+cards.forEach(function (card) {
+    let expander = card.querySelector('.js-expander');
+    if (expander) {
+        expander.addEventListener('click', function () {
+            toggle(card);
+        });
+    }
+});
+
+// temp
+console.log(cards)
