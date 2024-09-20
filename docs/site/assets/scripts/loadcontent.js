fef0e4f7fd70a1069c492e7b00320dec612e5bc7
Leonardo <leonardo.mf@ufu.br>
2024-09-20 15:39:06 -0300

    melhorias cards e responsividade

--- docs/site/assets/scripts/loadcontent.js
+++ docs/site/assets/scripts/loadcontent.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const cards = document.querySelectorAll('.card');
     const contentLoaded = 'content-loaded';
+    const display = 'display';
 
     cards.forEach(function (card) {
         let expander = card.querySelector('.js-expander');
         let contentSection = card.querySelector('.content-section');
+        let loadSpinner = card.querySelector('.loading');
+
         if (expander) {
             expander.addEventListener('click', function () {
                 if (!card.classList.contains(contentLoaded)) {
+                    loadSpinner.classList.add(display); // exibe o load
                     fetch('./content/'+ contentSection.id +'.html').then(function (response) {
                         if (response.ok){
                             return response.text();
@@ -18,7 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
                         //
                         throw response;
                     }).then(function (text) {
+                        loadSpinner.classList.remove(display); //remove o load
                         contentSection.innerHTML = text;
+                        card.classList.add(contentLoaded);
                     });
                 }
             });
